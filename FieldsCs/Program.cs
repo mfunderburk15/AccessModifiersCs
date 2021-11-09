b08e111c504722e2e6d9b22fd19814a91a28a43e
masen.funderburk@gmail.com <masen.funderburk@gmail.com>
2021-11-08 22:30:49 -0600

    first commit

---
+++ FieldsCs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FieldsCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
